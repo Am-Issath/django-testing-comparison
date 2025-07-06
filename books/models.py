5967403c0f943f4b8fa4b1bf8cfebd1535d674aa
issath-voisek <issath@voisek.com>
2025-07-07 00:03:55 +0530

    Created Book model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    published_year = models.IntegerField()
+    available = models.BooleanField(default=True)
